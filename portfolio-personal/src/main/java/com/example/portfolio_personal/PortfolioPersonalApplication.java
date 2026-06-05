6d42bd7294d52003a8385f8cf24afa513795fe24
David Escarcega <davidaescarcega@gmail.com>
2026-06-05 09:51:10 -0700

    porfolio personal

---
+++ portfolio-personal/src/main/java/com/example/portfolio_personal/PortfolioPersonalApplication.java
@@ -0,0 +1,13 @@
+package com.example.portfolio_personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioPersonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioPersonalApplication.class, args);
+	}
+
+}
